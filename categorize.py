05331f2bb679e95951f443f3bfea755f8be1fdcd
thesociallions <contact@thesociallions.nl>
2016-01-02 13:16:58 +0100

    This time, it should work...

--- categorize.py
+++ categorize.py
@@ -6,34 +6,36 @@ import pickle
 import sys
 import os
 
+
 def main():
-	provinces = ['Noord-Holland', 'Zuid-Holland', 'Noord-Brabant', 'Gelderland', 'Zeeland', 'Friesland', 'Utrecht', 'Limburg', 'Overijssel', 'Drenthe', 'Groningen', 'Flevoland']
-	tweets = pickle.load(open('tweets.pickle','rb'))
-	if not os.path.exists("collection"):
-		os.mkdir("collection")
-	os.chdir("collection")
-	for key,value in tweets.items():
-		filenameEnd = -1
-		if key in provinces:
-			if not os.path.exists(key):
-				os.makedirs(key)
-			os.chdir(key)
-			for tweet in value:
-				twitterID = tweet[0]
-				twitterUser = tweet[1]
-				twitterText = tweet[2]
-				twitterGEO	= tweet[3]
+    provinces = ['Noord-Holland', 'Zuid-Holland', 'Noord-Brabant', 'Gelderland', 'Zeeland', 'Friesland', 'Utrecht',
+                 'Limburg', 'Overijssel', 'Drenthe', 'Groningen', 'Flevoland']
+    tweets = pickle.load(open('tweets.pickle', 'rb'))
 
-				# Remove emoji's from tweet
- 				clearEmojis = tweet.encode("iso-8859-1", "ignore")
-				twitterText = clearEmojis.decode()
+    if not os.path.exists("collection"):
+        os.mkdir("collection")
+    os.chdir("collection")
 
-				filenameEnd += 1
-				filename = "art."+str(filenameEnd)
-				file = open(filename, "w", encoding="iso-8859-1")
-				file.write(twitterText)
-				file.close()
-			os.chdir("..")
+    for key, value in tweets.items():
+        filenameEnd = -1
+        if key in provinces:
+            if not os.path.exists(key):
+                os.makedirs(key)
+            os.chdir(key)
+            for tweet in value:
+                twitterText = tweet[2]
+
+                # Remove emoji's from tweet
+                clearEmojis = twitterText.encode("iso-8859-1", "ignore")
+                twitterText = clearEmojis.decode()
+
+                filenameEnd += 1
+                filename = "art." + str(filenameEnd)
+                file = open(filename, "w", encoding="iso-8859-1")
+                file.write(twitterText)
+                file.close()
+            os.chdir("..")
+
 
 if __name__ == "__main__":
-	main()
+    main()
